71a2c11f601c72d4ddebbdc556e330a858325638
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2022-02-13 00:06:32 +0600

    product page

--- src/product/Product.js
+++ src/product/Product.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./Product.css";
-// import { useStateValue } from "./StateProvider";
+import { useStateValue } from "../State/StateProvider";
 
 function Product({ id, title, image, price, rating }) {
-//   const [{ basket }, dispatch] = useStateValue();
-
+  const [{ basket }, dispatch] = useStateValue();
+  console.log(basket)
   const addToBasket = () => {
     // dispatch the item into the data layer
-    // dispatch({
-    //   type: "ADD_TO_BASKET",
-    //   item: {
-    //     id: id,
-    //     title: title,
-    //     image: image,
-    //     price: price,
-    //     rating: rating,
-    //   },
-    // });
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
   };
 
   return (
@@ -29,9 +29,9 @@ function Product({ id, title, image, price, rating }) {
         </p>
         <div className="product__rating">
           {Array(rating)
-            .fill()
+            .fill('🌟')
             .map((_, i) => (
-              <p>🌟</p>
+              <p>{_}</p>
             ))}
         </div>
       </div>
