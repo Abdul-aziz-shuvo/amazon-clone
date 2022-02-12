8cc5f7fa65fb7199b3cf65fb8ebd5fdadc308500
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2022-02-13 00:07:54 +0600

    global state

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {StateProvider} from "./State/StateProvider";
+import {initialState,reducer} from "./reducer/reducer";
+
 
 
 ReactDOM.render(
   <React.StrictMode>
-     <div className="app">
-    <App />
-    </div>
+      <StateProvider initialState={initialState} reducer={reducer}>
+         <div className="app">
+              <App />
+         </div>
+      </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/reducer/reducer.js
@@ -0,0 +1,14 @@
+export const initialState = {
+    basket : []
+}
+
+
+export const reducer = (state = initialState,action) => {
+    switch (action.type){
+        case  'ADD_TO_BASKET' :
+            return {...state,
+                basket: [...state.basket,action.item]}
+        default :
+            return state;
+    }
+}
